18dfb1cc50edceee0e4bd1eeade17500656ebdbc
Mike HP <tutoruialsspartan@gmail.com>
2024-08-08 16:29:28 -0600

    Welcome Route

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,5 +4,17 @@ export const routes: Routes = [
   {
     path:'',
     loadComponent:()=> import('./@pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path:'welcome',
+    loadComponent: () => import('./components/welcome/welcome.component').then(m => m.WelcomeComponent)
+  },
+  {
+    path:'login',
+    loadComponent: () => import('./@pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path:'register',
+    loadComponent: () => import('./@pages/register/register.component').then(m => m.RegisterComponent)
   }
 ];
