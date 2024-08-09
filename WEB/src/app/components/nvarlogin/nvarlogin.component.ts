44e4d6c66d46d56b5bc6080ee7b5f51c1eb07f10
Mike HP <tutoruialsspartan@gmail.com>
2024-08-09 02:01:01 -0600

    Posts

--- WEB/src/app/components/nvarlogin/nvarlogin.component.ts
+++ WEB/src/app/components/nvarlogin/nvarlogin.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './nvarlogin.component.scss'
 })
 export class NvarloginComponent {
-
+  logout(){
+    localStorage.clear()
+    
+  }
 }
--- WEB/src/app/components/posts/posts.component.ts
+++ WEB/src/app/components/posts/posts.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../../shared/interfaces';
 import { MatIconModule } from '@angular/material/icon';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule,DatePipe],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
