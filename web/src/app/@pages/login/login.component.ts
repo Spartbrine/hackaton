3be731e2ceebb9acf52ddbce50afd798851c5894
Mike HP <tutoruialsspartan@gmail.com>
2024-08-08 18:11:44 -0600

    Login & Register

--- web/src/app/@pages/login/login.component.ts
+++ web/src/app/@pages/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import {FormsModule } from '@angular/forms'
+import { User } from '../../shared/interfaces';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  ver_password = '';
+  user : User = {
+    id:0,
+    name:'',
+    email:'',
+    password:''
+  }
 
+  users : User[] = [];
 }
