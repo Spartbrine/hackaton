cacd97e665f0f2cd0b0f9974629feab8079efc27
Mike HP <tutoruialsspartan@gmail.com>
2024-08-08 23:41:33 -0600

    Home

---
+++ WEB/src/app/components/newpost/newpost.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+@Component({
+  selector: 'app-newpost',
+  standalone: true,
+  imports: [MatDialogModule],
+  templateUrl: './newpost.component.html',
+  styleUrl: './newpost.component.scss'
+})
+export class NewpostComponent {
+  dialog = inject(MatDialog)
+  openDialog(){
+    this.dialog.open(CreatePostDialogComponent)
+  }
+}
+
+@Component({
+  selector: 'app-create-post-dialog',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  templateUrl: './create-post-dialog.component.html',
+  styleUrl: './newpost.component.scss'
+})
+export class CreatePostDialogComponent {
+    readonly dialogRef = inject(MatDialogRef<CreatePostDialogComponent>);
+    
+}
